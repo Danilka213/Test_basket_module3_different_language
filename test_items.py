dcc0eceada6ee516a58e7a226a509632e54c1612
Danilka213 <55345403+Danilka213@users.noreply.github.com>
2021-07-25 16:27:31 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -6,10 +6,9 @@ import math
    
 class TestMainPage1(): 
     #@pytest.fixture
-    def test_check_basket(self, browser, choose_language):
-        link = f"https://selenium1py.pythonanywhere.com/{choose_language}/catalogue/coders-at-work_207/"
+    def test_check_basket(self, browser):
+        link = "https://selenium1py.pythonanywhere.com//catalogue/coders-at-work_207/"
         browser.get(link)
-        answer = math.log(int(time.time()))
         browser.implicitly_wait(10)
         assert browser.find_element_by_class_name("btn.btn-lg.btn-primary.btn-add-to-basket"), "Button 'basket' absent"
         time.sleep(7)
